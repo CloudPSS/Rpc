85f5cb6798f7437458691208112eff9a3d671cee
Opportunity <opportunity@live.in>
2021-10-15 12:47:42 +0800

    v0.1.0-dev.2

--- bin/build-thrift.js
+++ bin/build-thrift.js
@@ -2,7 +2,7 @@ const { spawn } = require('child_process');
 const path = require('path');
 
 if (process.platform !== 'win32') {
-    spawn(path.resolve(__dirname, 'build-thrift.sh'), {
+    spawn(path.resolve(__dirname, 'bash build-thrift.sh'), {
         stdio: 'inherit',
     }).on('close', (code) => {
         process.exit(code);
