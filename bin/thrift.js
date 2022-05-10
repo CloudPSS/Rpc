0593ca81c52a133c918ed26122a13b99713716aa
Opportunity <opportunity@live.in>
2022-05-10 12:42:08 +0800

    fix: make executable

--- bin/thrift.js
+++ bin/thrift.js
@@ -2,9 +2,11 @@
 
 const { spawn } = require('child_process');
 const path = require('path');
+const { chmodSync } = require('fs');
 
 const executable =
     process.platform !== 'win32' ? path.resolve(__dirname, 'thrift') : path.resolve(__dirname, 'thrift.exe');
+chmodSync(executable, '755');
 
 const args = process.argv.slice(2);
 spawn(executable, args, {
