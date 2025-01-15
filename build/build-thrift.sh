15c362fead49ff239c12efc324bba97160b6586f
OpportunityLiu <opportunity@live.in>
2025-01-15 15:02:49 +0800

    build: 更新构建脚本以使用中国镜像并支持指定版本

--- build/build-thrift.sh
+++ build/build-thrift.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
-# apt install flex bison cmake
+sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list
+sed -i 's/security.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
+sed -i 's/http:/https:/g' /etc/apt/sources.list
+apt-get update
+apt-get install -y cmake flex bison
 
 dir="$(dirname "$(realpath "$0")")/temp"
 
-apt-get update
-apt-get install -y cmake flex bison
 cd $dir
 rm -rf ./thrift ./thrift-*/
 tar -xf ./thrift.tar.gz
--- build/upgrade-bin.ps1
+++ build/upgrade-bin.ps1
@@ -1,3 +1,7 @@
+param(
+  [string]$version
+)
+
 $index = Invoke-WebRequest "https://dlcdn.apache.org/thrift"
 $versions = @(
   $index.Links 
@@ -5,14 +9,23 @@ $versions = @(
   | ForEach-Object { $_.href.SubString(0, $_.href.Length - 1) }
   | Sort-Object { [System.Version]$_ }
 )
-$lateset = $versions[-1]
 
-Write-Output "Latest versions of the thrift is $lateset"
+if ($version) {
+  $lateset = $versions | Where-Object { $_ -eq $version }
+  if (-not $lateset) {
+    Write-Output "Cannot find thrift version $version"
+    exit 1
+  }
+} else {
+  $lateset = $versions[-1]
+  Write-Output "Latest versions of the thrift is $lateset"
+}
+
 Invoke-WebRequest "https://dlcdn.apache.org/thrift/$lateset/thrift-$lateset.exe" -OutFile "$PSScriptRoot/../bin/thrift.exe"
 Write-Output "Downloaded thrift-$lateset.exe to bin/thrift.exe"
 
-Remove-Item "$PSScriptRoot/temp" -Force -Recurse
-New-Item "$PSScriptRoot/temp" -Type Directory
+Remove-Item "$PSScriptRoot/temp" -Force -Recurse | Out-Null
+New-Item "$PSScriptRoot/temp" -Type Directory | Out-Null
 Invoke-WebRequest "https://dlcdn.apache.org/thrift/$lateset/thrift-$lateset.tar.gz" -OutFile "$PSScriptRoot/temp/thrift.tar.gz"
 Write-Output "Downloaded thrift-$lateset.tar.gz to build/temp/thrift.tar.gz"
 
@@ -23,5 +36,5 @@ Write-Output "Copy thrift to bin/thrift"
 Remove-Item "$PSScriptRoot/temp" -Force -Recurse
 
 $pkg = Get-Content "$PSScriptRoot/../package.json" | ConvertFrom-Json
-$pkg.dependencies.thrift = "$lateset"
+$pkg.peerDependencies.thrift = "$lateset"
 ConvertTo-Json $pkg | Out-File "$PSScriptRoot/../package.json"
